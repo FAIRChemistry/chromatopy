5dbbb3efba145cf71128e1f754ece504f2d77f52
haeussma <83341109+haeussma@users.noreply.github.com>
2024-03-03 10:47:15 +0100

    added lc parser class

--- chromatopy/readers/abstractreader.py
+++ chromatopy/readers/abstractreader.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 from io import StringIO
 import os
 import pathlib
@@ -21,7 +22,7 @@ class AbstractReader(ABC):
             return [self.path]
 
     @abstractmethod
-    def read(self):
+    def read_file(self):
         raise NotImplementedError()
 
     @abstractmethod
@@ -48,7 +49,7 @@ class CSVReader(AbstractReader):
 
 class ChemStationReader(AbstractReader):
 
-    def read(self):
+    def read_file(self):
         pass
 
 
@@ -65,35 +66,51 @@ class ShimadzuReader(AbstractReader):
         else:
             return [self.path]
 
-    def read(self):
+    def read_file(self, path: str):
         """
         Reads the contents of one or multiple files and returns them as a list of strings.
 
         Returns:
             A list of strings, where each string represents the contents of a file.
         """
-        file_contents = []
-        for path in self._paths():
-            try:
-                file_contents.append(
-                    pathlib.Path(path).read_text(encoding="ISO-8859-1")
-                )
-            except IOError as e:
-                print(f"Error reading file: {path}")
-                raise e
+        content = pathlib.Path(path).read_text(encoding="ISO-8859-1")
+        sections = self._parse_sections(content)
 
-        sections = [self._parse_sections(content) for content in file_contents]
+        measurement_dict = self._map_measurement(sections)
+        peak_dict = self.extract_peaks(sections)
+        chromatogram_dict = self.extract_signal(sections)
+        chromatogram_dict["peaks"] = peak_dict
+        measurement_dict["Chromatograms"] = [chromatogram_dict]
 
-        peak_tables = [self.get_peak_table(section) for section in sections]
-        peaks = [self._map_peak_table(table) for table in peak_tables]
+        return measurement_dict
 
-        return peaks
+    def _map_measurement(self, sections: dict) -> dict:
+        header = self.get_header(sections)
+        timestamp = datetime.strptime(
+            f"{header['Output Date'].rstrip('.')} {header['Output Time']}",
+            "%d.%m.%Y %H:%M:%S",
+        )
+        sample_info = self.get_sample_information(sections)
+        dilution_factor = sample_info.get("Dilution Factor", 1)
+        injection_volume = sample_info.get("Injection Volume", None)
+        try:
+            injection_volume = float(injection_volume) / float(dilution_factor)
+        except TypeError:
+            raise ValueError("Injection volume not found in sample information")
 
-    def extract_peaks(self):
-        pass
+        return {
+            "timestamp": timestamp,
+            "injection_volume": injection_volume,
+            "injection_volume_unit": "µL",
+        }
 
-    def extract_signal(self):
-        pass
+    def extract_peaks(self, sections: dict):
+        table = self.get_peak_table(sections)
+        return self._map_peak_table(table)
+
+    def extract_signal(self, sections) -> dict:
+        table = self.get_chromatogram_table(sections)
+        return self._map_chromatogram_table(table)
 
     def _parse_sections(self, file_content: str) -> dict:
         """Parse a Shimadzu ASCII-export file into sections."""
@@ -110,12 +127,19 @@ class ShimadzuReader(AbstractReader):
 
     def parse_meta(self, sections: dict, section_name: str, nrows: int) -> dict:
         """Parse the metadata in a section as keys-values."""
-        meta_table = pd.read_table(
-            StringIO(sections[section_name]), nrows=nrows, header=None
-        )
-        meta = {row[0]: row[1] for _, row in meta_table.iterrows()}
 
-        return meta
+        meta_table = (
+            pd.read_table(
+                StringIO(sections[section_name]),
+                nrows=nrows,
+                header=None,
+                sep=",",
+            )
+            .set_index(0)[1]
+            .to_dict()
+        )
+
+        return meta_table
 
     def parse_table(
         self, sections: dict, section_name: str, skiprows: int = 1
@@ -124,7 +148,7 @@ class ShimadzuReader(AbstractReader):
         table_str = sections[section_name]
 
         # Count number of non-empty lines
-        num_lines = len([le for le in re.split("[\r\n]+", table_str) if len(le)])
+        num_lines = len(table_str.splitlines())
 
         if num_lines <= 1:
             return None
@@ -137,18 +161,42 @@ class ShimadzuReader(AbstractReader):
         area_col = "Area"
         peak_start_col = "I.Time"
         peak_end_col = "F.Time"
+        tailing_col = "Tailing"
+        separation_col = "Sep.Factor"
+        peak_start_col = "I.Time"
+        peak_end_col = "F.Time"
 
         return table.apply(
             lambda row: {
                 "retention_time": row[retention_time_col],
                 "retention_time_unit": "min",
+                "peak_start": row[peak_start_col],
+                "peak_end": row[peak_end_col],
                 "height": row[height_col],
                 "area": row[area_col],
                 "width": row[peak_end_col] - row[peak_start_col],
                 "width_unit": "min",
+                "tailing_factor": row[tailing_col],
+                "separation_factor": row[separation_col],
             },
             axis=1,
         ).tolist()
+
+    def _map_chromatogram_table(self, table: pd.DataFrame) -> dict:
+        """
+        Maps the chromatogram table to a dictionary format.
+
+        Args:
+            table (pd.DataFrame): The chromatogram table.
+
+        Returns:
+            dict: The mapped chromatogram dictionary.
+        """
+        return {
+            "retention_times": table["R.Time (min)"].tolist(),
+            "signals": table["Value (mV)"].tolist(),
+            "time_unit": "min",
+        }
 
     def get_peak_table(
         self, sections: dict, detector: str = "A-Ch1"
@@ -177,7 +225,7 @@ class ShimadzuReader(AbstractReader):
         section_name = f"LC Chromatogram(Detector {detector}-Ch{channel})"
 
         meta = self.parse_meta(sections, section_name, 6)
-        table = self.parse_table(sections, section_name, skiprows=6)
+        table = self.parse_table(sections, section_name, skiprows=7)
 
         # Convert intensity values into what they are supposed to be
         table["Value (mV)"] = table["Intensity"] * float(meta["Intensity Multiplier"])
