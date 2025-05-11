1845680f54694b8f71515e4048145ec8029fbc27
haeussma <83341109+haeussma@users.noreply.github.com>
2025-05-11 21:23:34 +0200

    Remove ontomaps.toml and refactor unit definitions in predefined.py to use string representations instead of UnitDefinition instances. This simplifies the unit handling and eliminates the need for the deleted units.py file.

--- chromatopy/units/predefined.py
+++ chromatopy/units/predefined.py
@@ -1,162 +1,48 @@
-from chromatopy.model import UnitType as ChromUnitType
-from chromatopy.tools.read_static import read_static_file
-
-from .units import BaseUnit, Prefix, UnitDefinition
-
-BaseUnit.model_rebuild()
-UnitDefinition.model_rebuild()
-
-ONTOMAPS = read_static_file("chromatopy.units", "ontomaps.toml")
-
-
-class Unit:
-    @staticmethod
-    def mol() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.MOLE, exponent=1, scale=1)
-
-    @staticmethod
-    def litre() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.LITRE, exponent=1, scale=1)
-
-    @staticmethod
-    def second() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.SECOND, exponent=1, scale=1)
-
-    @staticmethod
-    def minute() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.SECOND, exponent=1, scale=1, multiplier=60)
-
-    @staticmethod
-    def hour() -> BaseUnit:
-        hour = 60 * 60
-        return BaseUnit(kind=ChromUnitType.SECOND, exponent=1, scale=1, multiplier=hour)
-
-    @staticmethod
-    def day() -> BaseUnit:
-        day = 60**2 * 24
-        return BaseUnit(kind=ChromUnitType.SECOND, exponent=1, scale=1, multiplier=day)
-
-    @staticmethod
-    def gram() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.GRAM, exponent=1, scale=1)
-
-    @staticmethod
-    def kelvin() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.KELVIN, exponent=1, scale=1)
-
-    @staticmethod
-    def celsius() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.CELSIUS, exponent=1, scale=1)
-
-    @staticmethod
-    def dimensionless() -> BaseUnit:
-        return BaseUnit(kind=ChromUnitType.DIMENSIONLESS, exponent=1, scale=1)
-
-
-###### Single Prefixes ######
-
-k = Prefix.k
-m = Prefix.m
-u = Prefix.u
-n = Prefix.n
-
-##### Predefined units #####
-
-# Dimensionless
-dimensionless = UnitDefinition(base_units=[Unit.dimensionless()])
-
 # Molarity
-M = Unit.mol() / Unit.litre()
-mM = m * Unit.mol() / Unit.litre()
-uM = u * Unit.mol() / Unit.litre()
-nM = n * Unit.mol() / Unit.litre()
-
-## Ontology
-M.ld_id = ONTOMAPS["molarity"]["M"]
-mM.ld_id = ONTOMAPS["molarity"]["mM"]
-uM.ld_id = ONTOMAPS["molarity"]["uM"]
-nM.ld_id = ONTOMAPS["molarity"]["nM"]
+M = "mol / litre"
+mM = "mmol / litre"
+uM = "umol / litre"
+nM = "nmol / litre"
 
 # Mass Concentration
-g_L = Unit.gram() / Unit.litre()
-mg_L = m * Unit.gram() / Unit.litre()
-ug_L = u * Unit.gram() / Unit.litre()
-
-## Ontology
-g_L.ld_id = ONTOMAPS["mass_per_volume"]["mass_per_volume"]
-mg_L.ld_id = ONTOMAPS["mass_per_volume"]["mass_per_volume"]
-ug_L.ld_id = ONTOMAPS["mass_per_volume"]["mass_per_volume"]
+g_L = "g / litre"
+mg_L = "mg / litre"
+ug_L = "ug / litre"
 
 # Substance
-mol = UnitDefinition(base_units=[Unit.mol()])._get_name()
-mmol = UnitDefinition(base_units=[m * Unit.mol()])._get_name()
-umol = UnitDefinition(base_units=[u * Unit.mol()])._get_name()
-nmol = UnitDefinition(base_units=[n * Unit.mol()])._get_name()
-
-## Ontology
-mol.ld_id = ONTOMAPS["substance"]["mol"]
-mmol.ld_id = ONTOMAPS["substance"]["mmol"]
-umol.ld_id = ONTOMAPS["substance"]["umol"]
-nmol.ld_id = ONTOMAPS["substance"]["nmol"]
+mol = "mol"
+mmol = "mmol"
+umol = "umol"
+nmol = "nmol"
 
 # Mass
-gram = UnitDefinition(base_units=[Unit.gram()])._get_name()
-g = UnitDefinition(base_units=[Unit.gram()])._get_name()
-mg = UnitDefinition(base_units=[m * Unit.gram()])._get_name()
-ug = UnitDefinition(base_units=[u * Unit.gram()])._get_name()
-ng = UnitDefinition(base_units=[n * Unit.gram()])._get_name()
-kg = UnitDefinition(base_units=[k * Unit.gram()])._get_name()
-
-## Ontology
-g.ld_id = ONTOMAPS["mass"]["g"]
-gram.ld_id = ONTOMAPS["mass"]["g"]
-mg.ld_id = ONTOMAPS["mass"]["mg"]
-ug.ld_id = ONTOMAPS["mass"]["ug"]
-ng.ld_id = ONTOMAPS["mass"]["ng"]
+gram = "g"
+g = "g"
+mg = "mg"
+ug = "ug"
+ng = "ng"
+kg = "kg"
 
 # Volume
-litre = UnitDefinition(base_units=[Unit.litre()])._get_name()
-l = UnitDefinition(base_units=[Unit.litre()])._get_name()  # noqa: E741
-ml = UnitDefinition(base_units=[m * Unit.litre()])._get_name()
-ul = UnitDefinition(base_units=[u * Unit.litre()])._get_name()
-nl = UnitDefinition(base_units=[n * Unit.litre()])._get_name()
+litre = "l"
+l = "l"  # noqa: E741
+ml = "ml"
+ul = "ul"
+nl = "nl"
 
-## Ontology
-
-l.ld_id = ONTOMAPS["volume"]["litre"]
-litre.ld_id = ONTOMAPS["volume"]["litre"]
-ml.ld_id = ONTOMAPS["volume"]["ml"]
-ul.ld_id = ONTOMAPS["volume"]["ul"]
-nl.ld_id = ONTOMAPS["volume"]["nl"]
 
 # Time
-second = UnitDefinition(base_units=[Unit.second()])._get_name()
-s = UnitDefinition(base_units=[Unit.second()])._get_name()
-minute = UnitDefinition(base_units=[Unit.minute()])._get_name()
-min = UnitDefinition(base_units=[Unit.minute()])._get_name()
-hour = UnitDefinition(base_units=[Unit.hour()])._get_name()
-h = UnitDefinition(base_units=[Unit.hour()])._get_name()
-day = UnitDefinition(base_units=[Unit.day()])._get_name()
-d = UnitDefinition(base_units=[Unit.day()])._get_name()
-
-## Ontology
-s.ld_id = ONTOMAPS["time"]["s"]
-second.ld_id = ONTOMAPS["time"]["s"]
-minute.ld_id = ONTOMAPS["time"]["min"]
-min.ld_id = ONTOMAPS["time"]["min"]
-hour.ld_id = ONTOMAPS["time"]["hour"]
-h.ld_id = ONTOMAPS["time"]["hour"]
-day.ld_id = ONTOMAPS["time"]["day"]
-d.ld_id = ONTOMAPS["time"]["day"]
+second = "s"
+s = "s"
+minute = "min"
+min = "min"
+hour = "hour"
+h = "hour"
+day = "day"
+d = "day"
 
 # Temperature
-
-kelvin = UnitDefinition(base_units=[Unit.kelvin()])._get_name()
-K = UnitDefinition(base_units=[Unit.kelvin()])._get_name()
-celsius = UnitDefinition(base_units=[Unit.celsius()])._get_name()
-C = UnitDefinition(base_units=[Unit.celsius()])._get_name()
-
-## Ontology
-
-K.ld_id = ONTOMAPS["temperature"]["K"]
-kelvin.ld_id = ONTOMAPS["temperature"]["K"]
+kelvin = "K"
+K = "K"
+celsius = "C"
+C = "C"
